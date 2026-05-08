4ec68ae41b9802eb566d398301825384c4480e15
Dezzify <dezztobrut@gmail.com>
2026-05-08 10:20:02 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    botToken: '8741013211:AAGBW4n3ebT2TMx_ljzUF2-MWOxbHFNDu0M', // Token dari BotFather
+    ownerId: '8302651892',               // ID lu
+    channelId: '-1003564796583',         // ID Channel buat Force Sub
+    logChatId: '-1003868113122',         // ID Grup DezzifyPublicChat
+    logMsgId: 15,                        // ID Pesan buat dikomenin (Log)
+    channelUrl: 'https://t.me/ChannelDezz'
+};
